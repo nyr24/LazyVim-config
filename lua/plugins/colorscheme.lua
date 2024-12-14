f856a88bb3ca015b59c14a096f1ba90be2b78ae9
Roman Shmarov <romashmarov82@gmail.com>
2024-12-14 16:21:21 +0300

    no italic + bold in theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,30 @@
 return {
-  {
-    "catppuccin/nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      flavour = "mocha",
-      term_colors = true,
-      default_integrations = true,
-      transparent_background = true,
-    },
-  },
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     style = "moon",
-  --     transparent = true,
-  --   },
-  -- },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			flavour = "mocha",
+			term_colors = true,
+			default_integrations = true,
+			transparent_background = true,
+			no_italic = true, -- Force no italic
+			no_bold = true, -- Force no bold
+		},
+	},
+	-- {
+	--   "folke/tokyonight.nvim",
+	--   lazy = false,
+	--   priority = 1000,
+	--   opts = {
+	--     style = "moon",
+	--     transparent = true,
+	--   },
+	-- },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
