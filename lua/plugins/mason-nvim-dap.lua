423b1270bb4ce501052fd7ef379701017ebcecf8
Roman Shmarov <romashmarov82@gmail.com>
2024-12-31 20:24:11 +0300

    feat: nvim dap mason

--- lua/plugins/mason-nvim-dap.lua
+++ lua/plugins/mason-nvim-dap.lua
@@ -1,11 +1,30 @@
 return {
-  "jay-babu/mason-nvim-dap.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "mfussenegger/nvim-dap",
-  },
-  opts = {
-    handlers = {},
-  },
+	"jay-babu/mason-nvim-dap.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		require("mason-nvim-dap").setup({
+			handlers = {
+				function(config)
+					require("mason-nvim-dap").default_setup(config)
+				end,
+				codelldb = function(config)
+					config.args = function()
+						local out = {}
+						local args_str = vim.fn.input("Arguments: ")
+						if string.len(args_str) == 0 then
+							return out
+						end
+
+						out = vim.fn.split(args_str, " ")
+					end
+
+					require("mason-nvim-dap").default_setup(config)
+				end,
+			},
+		})
+	end,
 }
