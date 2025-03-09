beedd60433c9b31d6d4f18dd4448a384064500e5
nyr <romashmarov82@gmail.com>
2025-03-09 23:32:59 +0300

    refactor: dap config from extras package

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,3 +1,7 @@
 return {
-	"mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap",
+    keys = {
+        { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+        { "<leader>dO", function() require("dap").step_out() end, desc = "Step Out" },
+    }
 }
